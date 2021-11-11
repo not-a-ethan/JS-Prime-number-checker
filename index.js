17b13c4cf902de40a1959f3aade2f10485682c4f
Ethan-Master-Coding <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-10 21:52:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 console.log('type check() in the console with the number you want to check in the parentheses like this check(25) then press enter.')
 
-
 function check (number) {
-  for (const i = 2; number > i; i++) {
-    const numbercheck = number/i;
-    const check = Math.floor(number/i)
-
-    if (numbercheck === check){
-      return `${number} is not a prime`
-      break
+  for (let i = 2; i < number; i++) {
+     return `${number} is not a prime`
+    if (number % i === 0){
     }else{
-      return `${number} is a prime`
+      return `${number} is a prime (second)`
     }
   }
 }
