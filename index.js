a2fe26cb0206d13e816ce88bcc21e7cd252e59d8
coolprojects <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-12 13:11:21 +0000

    Fixed bugs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 console.log('type check() in the console with the number you want to check in the parentheses like this check(25) then press enter.')
 
-function check (number) {
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0){
-      return `${number} is not a prime`
+let composite = false
+let prime = false
+
+function check (num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0 && num !== i){
+      composite = true
     }else{
-      return `${number} is a prime`
+      prime = true
     }
+  }
+
+  if (composite) {
+    return `${num} is not a prime`
+  }else{
+    return `${num} is a prime`
   }
 }
