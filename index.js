466fa96c2575187c86f88c1c85c533a7ef0cb208
coolprojects <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-13 00:46:23 +0000

    Fixed a bug

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 console.log('type check() in the console with the number you want to check in the parentheses like this check(25) then press enter.')
 
-let composite = false
-let prime = false
-
 function check (num) {
+  let composite = false
   for (let i = 2; i < num; i++) {
     if (num % i === 0 && num !== i){
       composite = true
-    }else{
-      prime = true
     }
   }
 
