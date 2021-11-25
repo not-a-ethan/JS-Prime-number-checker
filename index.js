19560ba85d908fe67042a74eb9ba0fdf5a95d111
Ethan <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-25 15:12:30 +0000

    Added what number is can go into the number you imputed (if the number is composite(not prime))

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 console.log('type check() in the console with the number you want to check in the parentheses like this check(25) then press enter.')
-
+let number = 0
 function check (num) {
   let composite = false
   for (let i = 2; i < num; i++) {
     if (num % i === 0 && num !== i){
       composite = true
+      number = i
+      break
     }
   }
-
-  if (composite) {
-    return `${num} is not a prime`
+if (composite) {
+    return `${num} is not a prime. One of ${num}'s divisors is ${number}`
   }else{
     return `${num} is a prime`
-  }
+}
 }
